d06132590f19587867c2598e7eff5e68024d08f5
Samuel Kay <samuel-kay@401kay.fr>
2014-05-18 12:00:19 +0200

    Begin french translation

---
+++ l10n/fr_FR.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="13"/>
+        <source>About Tidings</source>
+        <translation>À propos de Tidings</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="38"/>
+        <source>A feed aggregator</source>
+        <translation type="unfinished">Un lecteur de flux rss</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>Version 0.1.0</source>
+        <translation>Version 0.1.0</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="73"/>
+        <source>Tidings is open source software licensed under the terms of </source>
+        <translation>Tidings est un logiciel libre distribué </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="74"/>
+        <source>sous licence GNU/GPL.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="84"/>
+        <source>View license</source>
+        <translation>Voir la licence</translation>
+    </message>
+</context>
+<context>
+    <name>FeedsPage</name>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="51"/>
+        <source>Tidings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="56"/>
+        <source>About Tidings</source>
+        <translation>À propos de Tidings</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="64"/>
+        <source>Refresh</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="74"/>
+        <source>Back to top</source>
+        <translation>Haut de page</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="149"/>
+        <source>No tidings is glad tidings?
+
+Please add some sources. →</source>
+        <translation>Pas de nouvelle, bonne nouvelle ?
+        
+        Ajoutez des flux. →</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../qml/pages/LicensePage.qml" line="18"/>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+</context>
+<context>
+    <name>SourceEditDialog</name>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="22"/>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Titre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="32"/>
+        <source>Enter name</source>
+        <translation>Saisissez le titre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="36"/>
+        <source>Source Address</source>
+        <translation>Adresse du flux</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="42"/>
+        <source>Enter URL</source>
+        <translation>Saisissez l'URL</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesPage</name>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="17"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="22"/>
+        <source>Add Source ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="41"/>
+        <source>Deleting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="101"/>
+        <source>Pull down to add RSS, Atom, or OPML sources.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="112"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ViewPage</name>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="57"/>
+        <source>Previous</source>
+        <translation>Précédent</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="57"/>
+        <source>Already at the beginning</source>
+        <translation>Article le plus récent</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="68"/>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="68"/>
+        <source>Article le plus ancient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="128"/>
+        <source>Full view</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebPage</name>
+    <message>
+        <location filename="../qml/pages/WebPage.qml" line="24"/>
+        <source>Open in browser</source>
+        <translation>Ouvrir dans le navigateur</translation>
+    </message>
+</context>
+</TS>
