19621610ca62de4f707ce537393ef377e5933d4e
Martin Grimme <martin.grimme@gmail.com>
2015-02-15 09:23:00 +0100

    improvements on the date string parser

--- src/dateparser.h
+++ src/dateparser.h
@@ -20,13 +20,34 @@ public:
         {
             // Qt::RFC2822Date is not supported by Qt < 5.2, so re-implement it
             // to stay backwards compatible
+
+            // non-numeric UTC offset representations
+            QMap<QString, long int> timezones;
+            timezones["UT"] = 0;
+            timezones["GMT"] = 0;
+            timezones["EST"] = -5 * 3600;
+            timezones["EDT"] = -4 * 3600;
+            timezones["CST"] = -6 * 3600;
+            timezones["CDT"] = -5 * 3600;
+            timezones["MST"] = -7 * 3600;
+            timezones["MDT"] = -6 * 3600;
+            timezones["PST"] = -8 * 3600;
+            timezones["PDT"] = -7 * 3600;
+
+            QStringList parts = dateString.split(' ');
+
             struct tm tm;
             setlocale(LC_TIME, "C");
             strptime(dateString.toLatin1().constData(),
                      "%a, %d %b %Y %H:%M:%S %z",
                      &tm);
             setlocale(LC_TIME, "");
-            d = QDateTime::fromTime_t(timegm(&tm));
+
+            // get the offset to UTC
+            long int utcOffset = timezones.value(parts.last(),
+                                                 tm.tm_gmtoff);
+            // treat time as UTC and correct timeshift by subtracting the offset
+            d = QDateTime::fromTime_t(timegm(&tm) - utcOffset);
         }
 
         if (! d.isValid())
