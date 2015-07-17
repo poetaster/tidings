a7a6b9bf970421ffc54bd0d2367d6b6d6f36668c
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-07-17 16:14:56 +0200

    Create harbour-tidings-sv.ts
    Swedish translation if wanted. Great app. Thanks!

---
+++ translations/harbour-tidings-sv.ts
@@ -0,0 +1,462 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="16"/>
+        <source>About Tidings</source>
+        <translation>Om Tidings</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="43"/>
+        <source>A feed aggregator</source>
+        <translation>En flödesaggregator</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="56"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="78"/>
+        <source>Tidings is open source software licensed under the terms of </source>
+        <translation>Tidings är öppen källkod, licensierad under villkoren för </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="79"/>
+        <source>the GNU General Public License.</source>
+        <translation>GNU General Public License.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="89"/>
+        <source>View license</source>
+        <translation>Visa licens</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="106"/>
+        <location filename="../qml/cover/CoverPage.qml" line="185"/>
+        <source>Refreshing</source>
+        <translation>Uppdaterar</translation>
+    </message>
+</context>
+<context>
+    <name>ExternalLinkDialog</name>
+    <message>
+        <location filename="../qml/pages/ExternalLinkDialog.qml" line="11"/>
+        <source>Open in browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+</context>
+<context>
+    <name>FeedsPage</name>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="74"/>
+        <source>%1 items</source>
+        <translation>%1 objekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="92"/>
+        <source>Sort by: %1</source>
+        <translation>Sortering: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="100"/>
+        <location filename="../qml/pages/FeedsPage.qml" line="104"/>
+        <source>All read</source>
+        <translation>Alla lästa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="114"/>
+        <source>Abort refreshing</source>
+        <translation>Avbryt uppdatering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="115"/>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="130"/>
+        <source>Stop Audio</source>
+        <translation>Stoppa ljud</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedsPage.qml" line="244"/>
+        <source>Pull down to add feeds.</source>
+        <translation>Dra neråt för att lägga till flöden.</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePage</name>
+    <message>
+        <location filename="../qml/pages/ImagePage.qml" line="19"/>
+        <source>Save to gallery</source>
+        <translation>Spara i galleriet</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../qml/pages/LicensePage.qml" line="20"/>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+</context>
+<context>
+    <name>LoadImagesButton</name>
+    <message>
+        <location filename="../qml/pages/LoadImagesButton.qml" line="13"/>
+        <source>Load images</source>
+        <translation>Läs in bilder</translation>
+    </message>
+</context>
+<context>
+    <name>MediaItem</name>
+    <message>
+        <location filename="../qml/pages/MediaItem.qml" line="84"/>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaItem.qml" line="86"/>
+        <source>Video</source>
+        <translation>Film</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaItem.qml" line="88"/>
+        <source>PDF document</source>
+        <translation>PDF-dokument</translation>
+    </message>
+</context>
+<context>
+    <name>NewsBlendModel</name>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="33"/>
+        <source>Latest first</source>
+        <translation>Senast först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="39"/>
+        <source>Oldest first</source>
+        <translation>Äldst först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="45"/>
+        <source>Feed, then latest first</source>
+        <translation>Flöde, sen senast först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="51"/>
+        <source>Feed, then oldest first</source>
+        <translation>Flöde, sen äldst först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="57"/>
+        <source>Current feed only, latest first</source>
+        <translation>Aktuellt flöde, senast först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="63"/>
+        <source>Current feed only, oldest first</source>
+        <translation>Aktuellt flöde, äldst först</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="313"/>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="319"/>
+        <location filename="../qml/pages/NewsBlendModel.qml" line="323"/>
+        <source>Error with %1:
+%2</source>
+        <translation>Fel med %1:
+%2</translation>
+    </message>
+</context>
+<context>
+    <name>ResourcesPage</name>
+    <message>
+        <location filename="../qml/pages/ResourcesPage.qml" line="33"/>
+        <source>Resources</source>
+        <translation>Resurser</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="17"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="27"/>
+        <source>Show preview images</source>
+        <translation>Förhandsvisa bilder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="28"/>
+        <source>Hiding the preview images saves network traffic. This switch does not affect embedded images in the item view.</source>
+        <translation>Att dölja förhandsvisning sparar nätverkstrafik. Detta val påverkar inte inbäddade bilder i objektvyn.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="45"/>
+        <source>Load images automatically</source>
+        <translation>Läs in bilder automatiskt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="46"/>
+        <source>If disabled, embedded images are not loaded automatically.</source>
+        <translation>Vid inaktivering läses inga inbäddade bilder in automatiskt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="63"/>
+        <source>Tinted items</source>
+        <translation>Tonade objekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="64"/>
+        <source>If enabled, items have their background tinted in their tag color.</source>
+        <translation>Vid aktivering tonas objektbakgrunder i respektive taggfärg.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="100"/>
+        <source>Font scale:</source>
+        <translation>Teckenskala:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="121"/>
+        <source>Apply font scale to web view</source>
+        <translation>Använd teckenskalan i webbvyn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="122"/>
+        <source>If enabled, the font scale settings apply to the web view.</source>
+        <translation>Vid aktivering används teckenskalans inställning i webbvyn.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="91"/>
+        <source>This setting changes the scale of the fonts in the feed view.</source>
+        <translation>Denna inställning ändrar teckenstorleken i flödesvyn.</translation>
+    </message>
+</context>
+<context>
+    <name>SortSelectorPage</name>
+    <message>
+        <location filename="../qml/pages/SortSelectorPage.qml" line="24"/>
+        <source>Sort by</source>
+        <translation>Sortera efter</translation>
+    </message>
+</context>
+<context>
+    <name>SourceEditDialog</name>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="31"/>
+        <source>Edit feed</source>
+        <translation>Redigera flöde</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="32"/>
+        <source>New feed</source>
+        <translation>Nytt flöde</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="36"/>
+        <source>Feed</source>
+        <translation>Flöde</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="42"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="43"/>
+        <source>Enter name</source>
+        <translation>Ange namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="50"/>
+        <source>Feed URL</source>
+        <translation>Flödes-URL</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="51"/>
+        <source>Enter URL</source>
+        <translation>Ange URL</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="81"/>
+        <source>Color tag</source>
+        <translation>Färgtagg</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="110"/>
+        <source>Database</source>
+        <translation>Databas</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="121"/>
+        <source>Clearing the read status will cause all available items to be reloaded the next time.</source>
+        <translation>Rensning av läst-status, orsakar ny inläsning av samtliga tillgängliga objekt nästa gång.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="132"/>
+        <source>Clear read status</source>
+        <translation>Rensa läst-status</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="152"/>
+        <source>Deleting will remove the feed and all of its items from the database.</source>
+        <translation>Borttagning tar bort flödet och alla dess objekt från databasen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceEditDialog.qml" line="163"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesPage</name>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="71"/>
+        <source>Feeds</source>
+        <translation>Flöden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="88"/>
+        <source>About Tidings</source>
+        <translation>Om Tidings</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="96"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="104"/>
+        <location filename="../qml/pages/SourcesPage.qml" line="108"/>
+        <location filename="../qml/pages/SourcesPage.qml" line="185"/>
+        <location filename="../qml/pages/SourcesPage.qml" line="348"/>
+        <source>All read</source>
+        <translation>Alla lästa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="118"/>
+        <source>Abort refreshing</source>
+        <translation>Avbryt uppdatering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="119"/>
+        <location filename="../qml/pages/SourcesPage.qml" line="338"/>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="134"/>
+        <source>Stop Audio</source>
+        <translation>Stoppa ljud</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="201"/>
+        <source>Clearing</source>
+        <translation>Rensar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="218"/>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="358"/>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+</context>
+<context>
+    <name>ViewPage</name>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="71"/>
+        <source>Audio</source>
+        <translation>Ljud</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="73"/>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="75"/>
+        <source>Video</source>
+        <translation>Film</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="77"/>
+        <source>PDF document</source>
+        <translation>PDF-dokument</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="208"/>
+        <source>Previous</source>
+        <translation>Föregående</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="209"/>
+        <source>Already at the beginning</source>
+        <translation>Redan vid början</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="239"/>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="240"/>
+        <source>Already at the end</source>
+        <translation>Redan vid slutet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="354"/>
+        <source>(%1 seconds)</source>
+        <translation>(%1 sekunder)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="427"/>
+        <source>Short article</source>
+        <translation>Kort artikel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="459"/>
+        <source>Website</source>
+        <translation>Webbsida</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="428"/>
+        <source>Full article</source>
+        <translation>Hel artikel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="486"/>
+        <source>Share</source>
+        <translation>Dela</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ViewPage.qml" line="529"/>
+        <source>Media</source>
+        <translation>Media</translation>
+    </message>
+</context>
+<context>
+    <name>WebPage</name>
+    <message>
+        <location filename="../qml/pages/WebPage.qml" line="41"/>
+        <source>Close web view</source>
+        <translation>Stäng webbvyn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WebPage.qml" line="50"/>
+        <source>Open in browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+</context>
+</TS>
